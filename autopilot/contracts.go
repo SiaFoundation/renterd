d59287bbb4975cc75352270e406062d88eb493e6
PJ <peterjan.brone@gmail.com>
2022-11-28 15:31:42 +0100

    autopilot: add TODO, remove unused methods

--- autopilot/contracts.go
+++ autopilot/contracts.go
@@ -3,7 +3,6 @@ package autopilot
 import (
 	"go.sia.tech/renterd/bus"
 	"go.sia.tech/renterd/internal/consensus"
-	"go.sia.tech/renterd/worker"
 	"go.sia.tech/siad/types"
 )
 
@@ -12,39 +11,15 @@ const (
 	defaultSetName = "autopilot"
 )
 
-func (ap *Autopilot) toWorkerContracts(contracts []bus.Contract) []worker.Contract {
-	out := make([]worker.Contract, 0, len(contracts))
-	for _, c := range contracts {
-		if c.ID == (types.FileContractID{}) || c.HostIP == "" {
-			continue
-		}
-		out = append(out, worker.Contract{
-			HostKey:   c.HostKey,
-			HostIP:    c.HostIP,
-			ID:        c.ID,
-			RenterKey: ap.deriveRenterKey(c.HostKey),
-		})
-	}
-	return out
-}
-
-func (ap *Autopilot) defaultContracts() ([]worker.Contract, error) {
-	cs, err := ap.bus.HostSetContracts(defaultSetName)
-	if err != nil {
-		return nil, err
-	}
-	return ap.toWorkerContracts(cs), nil
-}
-
 func (ap *Autopilot) updateDefaultContracts(active, toRenew, renewed, formed []bus.Contract, deleted []types.FileContractID) error {
 	// build some maps
 	isDeleted := make(map[types.FileContractID]bool)
 	for _, d := range deleted {
 		isDeleted[d] = true
 	}
-	isUpForRenewal := make(map[types.FileContractID]bool)
+	wasUpForRenewal := make(map[types.FileContractID]bool)
 	for _, r := range toRenew {
-		isUpForRenewal[r.ID] = true
+		wasUpForRenewal[r.ID] = true
 	}
 	isRenewed := make(map[types.FileContractID]bool)
 	for _, r := range renewed {
@@ -54,7 +29,11 @@ func (ap *Autopilot) updateDefaultContracts(active, toRenew, renewed, formed []b
 	// build new contract set
 	var contracts []consensus.PublicKey
 	for _, c := range append(active, formed...) {
-		if !isDeleted[c.ID] && !(isUpForRenewal[c.ID] && !isRenewed[c.ID]) {
+		// TODO: excluding contracts that are up for renewal but have not been
+		// renewed yet, we probably want the autopilot to manage more than one
+		// set of contracts (e.g. goodForUpload - goodForDownload contracts)
+		upForRenewal := wasUpForRenewal[c.ID] && !isRenewed[c.ID]
+		if !isDeleted[c.ID] && !upForRenewal {
 			contracts = append(contracts, c.HostKey)
 		}
 	}
