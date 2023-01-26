16b1de63e00f493441321c6599a48d29656d83eb
Chris Schinnerl <chris@sia.tech>
2023-01-26 15:02:17 +0100

    stores: implement accounts persistence

--- internal/stores/accounts.go
+++ internal/stores/accounts.go
@@ -6,6 +6,7 @@ import (
 	"go.sia.tech/core/types"
 	"go.sia.tech/renterd/api"
 	rhpv3 "go.sia.tech/renterd/rhp/v3"
+	"gorm.io/gorm/clause"
 )
 
 type (
@@ -29,10 +30,37 @@ func (dbAccount) TableName() string {
 	return "ephemeral_accounts"
 }
 
+// Accounts returns all accoutns from the db.
 func (s *SQLStore) Accounts() ([]api.Account, error) {
-	return []api.Account{}, nil // TODO: implement
+	var dbAccounts []dbAccount
+	if err := s.db.Find(&dbAccounts).Error; err != nil {
+		return nil, err
+	}
+	accounts := make([]api.Account, len(dbAccounts))
+	for i, acc := range dbAccounts {
+		accounts[i] = api.Account{
+			ID:      acc.AccountID,
+			Host:    acc.Host,
+			Balance: acc.Balance,
+			Owner:   acc.Owner,
+		}
+	}
+	return accounts, nil
 }
 
-func (s *SQLStore) SaveAccounts([]api.Account) error {
-	return nil // TODO: implement
+// SaveAccounts saves the given accounts in the db, overwriting any existing
+// ones.
+func (s *SQLStore) SaveAccounts(accounts []api.Account) error {
+	dbAccounts := make([]dbAccount, len(accounts))
+	for i, acc := range accounts {
+		dbAccounts[i] = dbAccount{
+			Owner:     acc.Owner,
+			AccountID: acc.ID,
+			Host:      acc.Host,
+			Balance:   acc.Balance,
+		}
+	}
+	return s.db.Clauses(clause.OnConflict{
+		UpdateAll: true,
+	}).Create(&dbAccounts).Error
 }
