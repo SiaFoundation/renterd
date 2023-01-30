a2f5aaef3d1bd14c04a19541da6b9ef845ded3fe
Chris Schinnerl <chris@sia.tech>
2023-01-30 13:07:55 +0100

    stores: update accountID to publicKey and Balance to string

--- internal/stores/accounts.go
+++ internal/stores/accounts.go
@@ -1,10 +1,7 @@
 package stores
 
 import (
-	"math/big"
-
 	"go.sia.tech/renterd/api"
-	rhpv3 "go.sia.tech/renterd/rhp/v3"
 )
 
 type (
@@ -14,13 +11,13 @@ type (
 		Owner string `gorm:"NOT NULL"`
 
 		// AccountID identifies an account. It's a public key.
-		AccountID rhpv3.Account `gorm:"unique;NOT NULL"`
+		AccountID publicKey `gorm:"unique;NOT NULL"`
 
 		// Host describes the host the account was created with.
 		Host publicKey `gorm:"NOT NULL"`
 
 		// Balance is the balance of the account.
-		Balance *big.Int `gorm:"type:bytes;serializer:gob"`
+		Balance string
 	}
 )
 
