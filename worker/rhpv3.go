6150a95f75462e8f6ab48f630023a202a9835cf7
PJ <peterjan.brone@gmail.com>
2023-03-14 15:14:17 +0100

    worker: fix nil check in pricetable updates

--- worker/rhpv3.go
+++ worker/rhpv3.go
@@ -312,13 +312,9 @@ type priceTable struct {
 	expiry time.Time
 
 	mu            sync.Mutex
-	ongoingUpdate *priceTableUpdate
-}
-
-type priceTableUpdate struct {
-	err  error
-	done chan struct{}
-	pt   *rhpv3.HostPriceTable
+	updateOngoing bool
+	updateChan    chan struct{}
+	updateErr     error
 }
 
 func newPriceTables() *priceTables {
@@ -345,14 +341,12 @@ func (pts *priceTables) Update(ctx context.Context, payFn PriceTablePaymentFunc,
 
 	// Check if there is some update going on already. If not, create one.
 	pt.mu.Lock()
-	ongoing := pt.ongoingUpdate
 	var performUpdate bool
-	if ongoing == nil {
-		ongoing = &priceTableUpdate{
-			done: make(chan struct{}),
-		}
-		pt.ongoingUpdate = ongoing
+	if !pt.updateOngoing {
 		performUpdate = true
+		pt.updateOngoing = true
+		pt.updateChan = make(chan struct{})
+		pt.updateErr = nil
 	}
 	pt.mu.Unlock()
 
@@ -362,12 +356,12 @@ func (pts *priceTables) Update(ctx context.Context, payFn PriceTablePaymentFunc,
 		select {
 		case <-ctx.Done():
 			return rhpv3.HostPriceTable{}, errors.New("timeout while blocking for pricetable update")
-		case <-ongoing.done:
+		case <-pt.updateChan:
 		}
-		if ongoing.err != nil {
-			return rhpv3.HostPriceTable{}, ongoing.err
+		if pt.updateErr != nil {
+			return rhpv3.HostPriceTable{}, pt.updateErr
 		} else {
-			return *ongoing.pt, nil
+			return *pt.pt, nil
 		}
 	}
 
@@ -386,10 +380,11 @@ func (pts *priceTables) Update(ctx context.Context, payFn PriceTablePaymentFunc,
 		pt.pt = &hpt
 		pt.expiry = time.Now().Add(hpt.Validity)
 	}
+
 	// Signal that the update is over.
-	ongoing.err = err
-	close(ongoing.done)
-	pt.ongoingUpdate = nil
+	pt.updateErr = err
+	pt.updateOngoing = false
+	close(pt.updateChan)
 	return hpt, err
 }
 
@@ -398,14 +393,11 @@ func (pts *priceTables) Update(ctx context.Context, payFn PriceTablePaymentFunc,
 func (pts *priceTables) priceTable(hk types.PublicKey) *priceTable {
 	pts.mu.Lock()
 	defer pts.mu.Unlock()
-	pt, exists := pts.priceTables[hk]
-	if !exists {
-		pt = &priceTable{
-			hk: hk,
-		}
-		pts.priceTables[hk] = pt
+
+	if _, exists := pts.priceTables[hk]; !exists {
+		pts.priceTables[hk] = &priceTable{hk: hk}
 	}
-	return pt
+	return pts.priceTables[hk]
 }
 
 // preparePriceTableContractPayment prepare a payment function to pay for a
