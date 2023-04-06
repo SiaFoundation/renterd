5526b09547545e5fbedc6ae6671d25f2bf9dc678
PJ <peterjan.brone@gmail.com>
2023-04-06 16:06:58 +0200

    testing: fix NDF in TestHostPruning

--- internal/testing/pruning_test.go
+++ internal/testing/pruning_test.go
@@ -18,7 +18,7 @@ func TestHostPruning(t *testing.T) {
 	ctx := context.Background()
 
 	// create a new test cluster
-	cluster, err := newTestCluster(t.TempDir(), newTestLoggerCustom(zapcore.DebugLevel))
+	cluster, err := newTestCluster(t.TempDir(), newTestLoggerCustom(zapcore.ErrorLevel))
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -58,26 +58,31 @@ func TestHostPruning(t *testing.T) {
 		t.Fatal(err)
 	}
 
+	// create a helper function to record a number of failed interactions
+	now := time.Now()
+	recordFailedInteractions := func() {
+		his := make([]hostdb.Interaction, 10)
+		for i := 0; i < 10; i++ {
+			now = now.Add(time.Hour)
+			his[i] = hostdb.Interaction{
+				Host:      hk1,
+				Timestamp: now,
+				Success:   false,
+				Type:      hostdb.InteractionTypeScan,
+			}
+		}
+		if err = b.RecordInteractions(context.Background(), his); err != nil {
+			t.Fatal(err)
+		}
+	}
+
 	// record a number of failed interaction for h1 that push his downtime well
 	// over the default 'MaxDowntimeHours' setting of 10 hours and ensures we
 	// push it over the 'MinRecentScanFailures' limit
 	//
 	// NOTE: we do this manually to avoid bypassing the condition by adding
 	// flags or extending the config with properties only used in testing
-	now := time.Now()
-	his := make([]hostdb.Interaction, 20)
-	for i := 0; i < 20; i++ {
-		now = now.Add(time.Hour * 20)
-		his[i] = hostdb.Interaction{
-			Host:      hk1,
-			Timestamp: now,
-			Success:   false,
-			Type:      hostdb.InteractionTypeScan,
-		}
-	}
-	if err = b.RecordInteractions(context.Background(), his); err != nil {
-		t.Fatal(err)
-	}
+	recordFailedInteractions()
 
 	// wait until we have 2 hosts in the set - we expect one host to be removed
 	// by the host pruning
@@ -87,6 +92,7 @@ func TestHostPruning(t *testing.T) {
 		if err != nil {
 			t.Fatal(err)
 		} else if len(hosts) != 2 {
+			recordFailedInteractions()
 			return fmt.Errorf("unexpected number of hosts, %v != 2", len(hosts))
 		}
 		return nil
