37f92906618b69bc39b579c42403c0a07c4ee8f0
PJ <peterjan.brone@gmail.com>
2023-04-18 10:17:50 +0200

    api: add api.ErrConsensusNotSynced

--- worker/worker.go
+++ worker/worker.go
@@ -803,9 +803,8 @@ func (w *worker) slabMigrateHandler(jc jape.Context) {
 
 	// cancel the upload if consensus is not synced
 	if !up.ConsensusState.Synced {
-		err = errors.New("consensus is not synced")
-		w.logger.Errorf("migration cancelled, err: ", err)
-		jc.Error(err, http.StatusServiceUnavailable)
+		w.logger.Errorf("migration cancelled, err: ", api.ErrConsensusNotSynced)
+		jc.Error(api.ErrConsensusNotSynced, http.StatusServiceUnavailable)
 		return
 	}
 
@@ -1010,9 +1009,8 @@ func (w *worker) objectsHandlerPUT(jc jape.Context) {
 
 	// cancel the upload if consensus is not synced
 	if !up.ConsensusState.Synced {
-		err = errors.New("consensus is not synced")
-		w.logger.Errorf("upload cancelled, err: ", err)
-		jc.Error(err, http.StatusServiceUnavailable)
+		w.logger.Errorf("upload cancelled, err: ", api.ErrConsensusNotSynced)
+		jc.Error(api.ErrConsensusNotSynced, http.StatusServiceUnavailable)
 		return
 	}
 
