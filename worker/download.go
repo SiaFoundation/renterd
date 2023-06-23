04d995245eae5881cde92eef335236d70a40cc7b
PJ <peterjan.brone@gmail.com>
2023-06-23 11:08:27 +0200

    worker: dont track 0 bytes dl

--- worker/download.go
+++ worker/download.go
@@ -549,7 +549,7 @@ func (d *downloader) processBatch(batch []*sectorDownloadReq) chan struct{} {
 	var concurrent int64
 	var downloadedB int64
 	trackStatsFn := func() {
-		if start.IsZero() || time.Since(start).Milliseconds() == 0 {
+		if start.IsZero() || time.Since(start).Milliseconds() == 0 || downloadedB == 0 {
 			return
 		}
 		durationMS := time.Since(start).Milliseconds()
