5b6ffd865811cf8aab99c3d8ed2c463e6fa4dcc3
Chris Schinnerl <chris@sia.tech>
2023-07-03 15:06:47 +0200

    object: add streamWriter and streamReader

--- object/object.go
+++ object/object.go
@@ -2,10 +2,11 @@ package object
 
 import (
 	"bytes"
-	"crypto/cipher"
+	"encoding/binary"
 	"encoding/hex"
 	"errors"
 	"io"
+	"math"
 
 	"golang.org/x/crypto/chacha20"
 	"lukechampine.com/frand"
@@ -38,19 +39,26 @@ func (k *EncryptionKey) UnmarshalText(b []byte) error {
 }
 
 // Encrypt returns a cipher.StreamReader that encrypts r with k.
-func (k EncryptionKey) Encrypt(r io.Reader) cipher.StreamReader {
+func (k EncryptionKey) Encrypt(r io.Reader) *streamReader {
 	c, _ := chacha20.NewUnauthenticatedCipher(k.entropy[:], make([]byte, 24))
-	return cipher.StreamReader{S: c, R: r}
+	return &streamReader{key: k.entropy[:], c: c, r: r}
 }
 
 // Decrypt returns a cipher.StreamWriter that decrypts w with k, starting at the
 // specified offset.
-func (k EncryptionKey) Decrypt(w io.Writer, offset uint64) cipher.StreamWriter {
-	c, _ := chacha20.NewUnauthenticatedCipher(k.entropy[:], make([]byte, 24))
+func (k EncryptionKey) Decrypt(w io.Writer, offset uint64) *streamWriter {
+	var nonce uint64
+	for offset > 64*math.MaxUint32 {
+		offset -= 64 * math.MaxUint32
+		nonce++
+	}
+	n := make([]byte, 24)
+	binary.LittleEndian.PutUint64(n[16:], nonce)
+	c, _ := chacha20.NewUnauthenticatedCipher(k.entropy[:], n)
 	c.SetCounter(uint32(offset / 64))
 	var buf [64]byte
 	c.XORKeyStream(buf[:offset%64], buf[:offset%64])
-	return cipher.StreamWriter{S: c, W: w}
+	return &streamWriter{key: k.entropy[:], c: c, w: w, counter: offset, nonce: nonce}
 }
 
 // GenerateEncryptionKey returns a random encryption key.
@@ -84,7 +92,7 @@ func (o Object) Size() int64 {
 
 // Encrypt wraps the given reader with a reader that encrypts the stream using
 // the object's key.
-func (o Object) Encrypt(r io.Reader) cipher.StreamReader {
+func (o Object) Encrypt(r io.Reader) *streamReader {
 	return o.Key.Encrypt(r)
 }
 
@@ -120,4 +128,84 @@ func SplitSlabs(slabs []Slab, lengths []int) [][]SlabSlice {
 // with the specified length.
 func SingleSlabs(slabs []Slab, length int) []SlabSlice {
 	return SplitSlabs(slabs, []int{length})[0]
+}
+
+type streamReader struct {
+	key []byte
+	c   *chacha20.Cipher
+	r   io.Reader
+
+	counter uint64 // bytes, not blocks
+	nonce   uint64
+}
+
+func (sr *streamReader) Read(p []byte) (int, error) {
+	n, err := sr.r.Read(p)
+	sr.counter += uint64(n)
+	if sr.counter < 64*math.MaxUint32 {
+		sr.c.XORKeyStream(p[:n], p[:n])
+		return n, err
+	}
+	// counter overflow; xor remaining bytes, then increment nonce and xor again
+	rem := 64*math.MaxUint32 - (sr.counter - uint64(n))
+	sr.counter -= 64 * math.MaxUint32
+	sr.c.XORKeyStream(p[:rem], p[:rem])
+	// NOTE: we increment the last 8 bytes because XChaCha uses the
+	// first 16 bytes to derive a new key; leaving them alone means
+	// the key will be stable, which might be useful.
+	sr.nonce++
+	nonce := make([]byte, 24)
+	binary.LittleEndian.PutUint64(nonce[16:], sr.nonce)
+	sr.c, _ = chacha20.NewUnauthenticatedCipher(sr.key, nonce)
+	sr.c.XORKeyStream(p[rem:], p[rem:])
+	return n, err
+}
+
+type streamWriter struct {
+	key []byte
+	c   *chacha20.Cipher
+	w   io.Writer
+
+	counter uint64
+	nonce   uint64
+}
+
+func (sw *streamWriter) Write(src []byte) (n int, err error) {
+	c := make([]byte, len(src))
+	if sw.counter+uint64(len(src)) < 64*math.MaxUint32 {
+		sw.c.XORKeyStream(c, src)
+		n, err = sw.w.Write(c)
+		sw.counter += uint64(n)
+		if n != len(src) && err == nil { // should never happen
+			err = io.ErrShortWrite
+		}
+		return
+	}
+	// counter overflow, xor remaining bytes, then increment nonce and xor again
+	rem := 64*math.MaxUint32 - sw.counter
+	sw.counter -= 64 * math.MaxUint32
+	sw.c.XORKeyStream(c[:rem], src[:rem])
+	// NOTE: we increment the last 8 bytes because XChaCha uses the
+	// first 16 bytes to derive a new key; leaving them alone means
+	// the key will be stable, which might be useful.
+	sw.nonce++
+	nonce := make([]byte, 24)
+	binary.LittleEndian.PutUint64(nonce[16:], sw.nonce)
+	sw.c, _ = chacha20.NewUnauthenticatedCipher(sw.key, nonce)
+	sw.c.XORKeyStream(c[rem:], src[rem:])
+	n, err = sw.w.Write(c)
+	sw.counter += uint64(n)
+	if n != len(src) && err == nil { // should never happen
+		err = io.ErrShortWrite
+	}
+	return
+}
+
+// Close closes the underlying Writer and returns its Close return value, if the Writer
+// is also an io.Closer. Otherwise it returns nil.
+func (w *streamWriter) Close() error {
+	if c, ok := w.w.(io.Closer); ok {
+		return c.Close()
+	}
+	return nil
 }
