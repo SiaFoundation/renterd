64a8c717a12bfdb6df1ae2c44dbe55a6cfbcb8d5
Christopher Schinnerl <chris@sia.tech>
2023-08-02 14:57:11 +0200

    Add Health field to Slab (#512)
    * objects: add Health field to Slab
    
    * stores: fix TestObjectBasic
    
    * stores: fix TestSQLMetadataStore

--- stores/metadata_test.go
+++ stores/metadata_test.go
@@ -61,6 +61,7 @@ func TestObjectBasic(t *testing.T) {
 		Slabs: []object.SlabSlice{
 			{
 				Slab: object.Slab{
+					Health:    1.0,
 					Key:       object.GenerateEncryptionKey(),
 					MinShards: 1,
 					Shards: []object.Sector{
@@ -75,6 +76,7 @@ func TestObjectBasic(t *testing.T) {
 			},
 			{
 				Slab: object.Slab{
+					Health:    1.0,
 					Key:       object.GenerateEncryptionKey(),
 					MinShards: 2,
 					Shards: []object.Sector{
@@ -873,6 +875,7 @@ func TestSQLMetadataStore(t *testing.T) {
 		Slabs: []object.SlabSlice{
 			{
 				Slab: object.Slab{
+					Health:    1,
 					Key:       object.GenerateEncryptionKey(),
 					MinShards: 1,
 					Shards: []object.Sector{
@@ -887,6 +890,7 @@ func TestSQLMetadataStore(t *testing.T) {
 			},
 			{
 				Slab: object.Slab{
+					Health:    1,
 					Key:       object.GenerateEncryptionKey(),
 					MinShards: 2,
 					Shards: []object.Sector{
