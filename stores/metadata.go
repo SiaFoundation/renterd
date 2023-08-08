3ab11bdd0c2b9894f27d2bebec79d072baf11f34
Chris Schinnerl <chris@sia.tech>
2023-08-08 15:55:36 +0200

    stores: try using ORDER BY in ObjectEntries

--- stores/metadata.go
+++ stores/metadata.go
@@ -5,7 +5,6 @@ import (
 	"errors"
 	"fmt"
 	"math"
-	"sort"
 	"strings"
 	"time"
 	"unicode/utf8"
@@ -728,6 +727,7 @@ FROM (
 ) AS m
 GROUP BY name
 HAVING ? AND name != ?
+ORDER BY name ASC
 LIMIT ? OFFSET ?`,
 		sqlConcat(s.db, "?", "trimmed"),
 		sqlConcat(s.db, "?", "substr(trimmed, 1, slashindex)")),
@@ -745,10 +745,6 @@ LIMIT ? OFFSET ?`,
 	if err != nil {
 		return nil, err
 	}
-	// Sort results outside of query to get consistent ordering across dbs.
-	sort.Slice(metadata, func(i, j int) bool {
-		return metadata[i].Name < metadata[j].Name
-	})
 	return metadata, nil
 }
 
