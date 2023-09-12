751733dc883cf37cd490b471417fa36c53ae06c0
Chris Schinnerl <chris@sia.tech>
2023-09-12 14:53:02 +0200

    docker: extend entrypoint script

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ "$BUILD_TAGS" == *'testnet'* ]]; then
-    exec renterd -http=':9880' "$@"
+    exec renterd -http=':9880' -s3.address=':7070' "$@"
 else
-    exec renterd -http=':9980' "$@"
+    exec renterd -http=':9980' -s3.address=':8080' "$@"
 fi
