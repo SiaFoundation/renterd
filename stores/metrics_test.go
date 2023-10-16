45631a42519d29f6d35a470f7e57c2043f1b8709
Chris Schinnerl <chris@sia.tech>
2023-10-16 12:12:52 +0200

    stores: implement contract set churn metrics

--- stores/metrics_test.go
+++ stores/metrics_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 	"time"
 
+	"go.sia.tech/core/types"
 	"go.sia.tech/renterd/api"
 )
 
@@ -68,4 +69,104 @@ func TestContractSetMetrics(t *testing.T) {
 	after := time.UnixMilli(1)  // 'after' is exclusive
 	before := time.UnixMilli(2) // 'before' is inclusive
 	assertMetrics(api.ContractSetMetricsQueryOpts{After: after, Before: before}, 1, []int{3})
+}
+
+func TestContractChurnSetMetrics(t *testing.T) {
+	ss := newTestSQLStore(t, defaultTestSQLStoreConfig)
+	defer ss.Close()
+
+	// Create metrics to query.
+	sets := []string{"foo", "bar"}
+	directions := []string{api.ChurnDirAdded, api.ChurnDirRemoved}
+	reasons := []string{"reasonA", "reasonB"}
+	times := []time.Time{time.UnixMilli(3), time.UnixMilli(1), time.UnixMilli(2)}
+	var i byte
+	for _, set := range sets {
+		for _, dir := range directions {
+			for _, reason := range reasons {
+				for _, recordedTime := range times {
+					fcid := types.FileContractID{i}
+					if err := ss.RecordContractSetChurnMetric(context.Background(), recordedTime, set, dir, reason, fcid); err != nil {
+						t.Fatal(err)
+					}
+					i++
+				}
+			}
+		}
+	}
+
+	// Query without any filters.
+	metrics, err := ss.contractSetChurnMetrics(context.Background(), api.ContractSetChurnMetricsQueryOpts{})
+	if err != nil {
+		t.Fatal(err)
+	} else if len(metrics) != 24 {
+		t.Fatalf("expected 24 metrics, got %v", len(metrics))
+	} else if !sort.SliceIsSorted(metrics, func(i, j int) bool {
+		return time.Time(metrics[i].Time).Before(time.Time(metrics[j].Time))
+	}) {
+		t.Fatal("expected metrics to be sorted by time")
+	}
+
+	// Query by set name.
+	metrics, err = ss.contractSetChurnMetrics(context.Background(), api.ContractSetChurnMetricsQueryOpts{
+		Name: sets[0],
+	})
+	if err != nil {
+		t.Fatal(err)
+	} else if len(metrics) != 12 {
+		t.Fatalf("expected 12 metrics, got %v", len(metrics))
+	}
+	for _, m := range metrics {
+		if m.Name != sets[0] {
+			t.Fatalf("expected name to be %v, got %v", sets[0], m.Name)
+		}
+	}
+
+	// Query by time.
+	after := time.UnixMilli(2)  // 'after' is exclusive
+	before := time.UnixMilli(3) // 'before' is inclusive
+	metrics, err = ss.contractSetChurnMetrics(context.Background(), api.ContractSetChurnMetricsQueryOpts{
+		After:  after,
+		Before: before,
+	})
+	if err != nil {
+		t.Fatal(err)
+	} else if len(metrics) != 8 {
+		t.Fatalf("expected 8 metrics, got %v", len(metrics))
+	}
+	for _, m := range metrics {
+		if m.Time != unixTimeMS(before) {
+			t.Fatalf("expected time to be %v, got %v", before, time.Time(m.Time).UnixMilli())
+		}
+	}
+
+	// Query by direction.
+	metrics, err = ss.contractSetChurnMetrics(context.Background(), api.ContractSetChurnMetricsQueryOpts{
+		Direction: directions[1],
+	})
+	if err != nil {
+		t.Fatal(err)
+	} else if len(metrics) != 12 {
+		t.Fatalf("expected 12 metrics, got %v", len(metrics))
+	}
+	for _, m := range metrics {
+		if m.Direction != directions[1] {
+			t.Fatalf("expected direction to be %v, got %v", directions[1], m.Direction)
+		}
+	}
+
+	// Query by reason.
+	metrics, err = ss.contractSetChurnMetrics(context.Background(), api.ContractSetChurnMetricsQueryOpts{
+		Reason: reasons[1],
+	})
+	if err != nil {
+		t.Fatal(err)
+	} else if len(metrics) != 12 {
+		t.Fatalf("expected 12 metrics, got %v", len(metrics))
+	}
+	for _, m := range metrics {
+		if m.Reason != reasons[1] {
+			t.Fatalf("expected reason to be %v, got %v", reasons[1], m.Reason)
+		}
+	}
 }
