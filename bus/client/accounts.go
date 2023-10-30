3ea0663a20a0350d54ea157f5d9e0de41a6fc197
Peter-Jan Brone <peterjan.brone@gmail.com>
2023-10-30 12:10:10 +0100

    Refactor Bus Routes (#668)
    * bus: fix route consistency
    
    * bus: fix bucket route
    
    * debug: add logging
    
    * Revert "debug: add logging"
    
    This reverts commit b560a7cfd770871275603f6571bacb7490e0d43b.
    
    * accounts: fix bad merge
    
    * bus: add old routes to ease UI switch

--- bus/client/accounts.go
+++ bus/client/accounts.go
@@ -13,7 +13,7 @@ import (
 
 // Account returns the account for given id.
 func (c *Client) Account(ctx context.Context, id rhpv3.Account, host types.PublicKey) (account api.Account, err error) {
-	err = c.c.WithContext(ctx).POST(fmt.Sprintf("/accounts/%s", id), api.AccountHandlerPOST{
+	err = c.c.WithContext(ctx).POST(fmt.Sprintf("/account/%s", id), api.AccountHandlerPOST{
 		HostKey: host,
 	}, &account)
 	return
@@ -27,7 +27,7 @@ func (c *Client) Accounts(ctx context.Context) (accounts []api.Account, err erro
 
 // AddBalance adds the given amount to an account's balance, the amount can be negative.
 func (c *Client) AddBalance(ctx context.Context, id rhpv3.Account, hk types.PublicKey, amount *big.Int) (err error) {
-	err = c.c.WithContext(ctx).POST(fmt.Sprintf("/accounts/%s/add", id), api.AccountsAddBalanceRequest{
+	err = c.c.WithContext(ctx).POST(fmt.Sprintf("/account/%s/add", id), api.AccountsAddBalanceRequest{
 		HostKey: hk,
 		Amount:  amount,
 	}, nil)
@@ -37,7 +37,7 @@ func (c *Client) AddBalance(ctx context.Context, id rhpv3.Account, hk types.Publ
 // LockAccount locks an account.
 func (c *Client) LockAccount(ctx context.Context, id rhpv3.Account, hostKey types.PublicKey, exclusive bool, duration time.Duration) (account api.Account, lockID uint64, err error) {
 	var resp api.AccountsLockHandlerResponse
-	err = c.c.WithContext(ctx).POST(fmt.Sprintf("/accounts/%s/lock", id), api.AccountsLockHandlerRequest{
+	err = c.c.WithContext(ctx).POST(fmt.Sprintf("/account/%s/lock", id), api.AccountsLockHandlerRequest{
 		HostKey:   hostKey,
 		Exclusive: exclusive,
 		Duration:  api.DurationMS(duration),
@@ -47,13 +47,13 @@ func (c *Client) LockAccount(ctx context.Context, id rhpv3.Account, hostKey type
 
 // ResetDrift resets the drift of an account to zero.
 func (c *Client) ResetDrift(ctx context.Context, id rhpv3.Account) (err error) {
-	err = c.c.WithContext(ctx).POST(fmt.Sprintf("/accounts/%s/resetdrift", id), nil, nil)
+	err = c.c.WithContext(ctx).POST(fmt.Sprintf("/account/%s/resetdrift", id), nil, nil)
 	return
 }
 
 // SetBalance sets the given account's balance to a certain amount.
 func (c *Client) SetBalance(ctx context.Context, id rhpv3.Account, hk types.PublicKey, amount *big.Int) (err error) {
-	err = c.c.WithContext(ctx).POST(fmt.Sprintf("/accounts/%s/update", id), api.AccountsUpdateBalanceRequest{
+	err = c.c.WithContext(ctx).POST(fmt.Sprintf("/account/%s/update", id), api.AccountsUpdateBalanceRequest{
 		HostKey: hk,
 		Amount:  amount,
 	}, nil)
@@ -62,7 +62,7 @@ func (c *Client) SetBalance(ctx context.Context, id rhpv3.Account, hk types.Publ
 
 // ScheduleSync sets the requiresSync flag of an account.
 func (c *Client) ScheduleSync(ctx context.Context, id rhpv3.Account, hk types.PublicKey) (err error) {
-	err = c.c.WithContext(ctx).POST(fmt.Sprintf("/accounts/%s/requiressync", id), api.AccountsRequiresSyncRequest{
+	err = c.c.WithContext(ctx).POST(fmt.Sprintf("/account/%s/requiressync", id), api.AccountsRequiresSyncRequest{
 		HostKey: hk,
 	}, nil)
 	return
@@ -70,7 +70,7 @@ func (c *Client) ScheduleSync(ctx context.Context, id rhpv3.Account, hk types.Pu
 
 // UnlockAccount unlocks an account.
 func (c *Client) UnlockAccount(ctx context.Context, id rhpv3.Account, lockID uint64) (err error) {
-	err = c.c.WithContext(ctx).POST(fmt.Sprintf("/accounts/%s/unlock", id), api.AccountsUnlockHandlerRequest{
+	err = c.c.WithContext(ctx).POST(fmt.Sprintf("/account/%s/unlock", id), api.AccountsUnlockHandlerRequest{
 		LockID: lockID,
 	}, nil)
 	return
--- bus/client/bucket.go
+++ bus/client/bucket.go
@@ -7,6 +7,12 @@ import (
 	"go.sia.tech/renterd/api"
 )
 
+// Bucket returns information about a specific bucket.
+func (c *Client) Bucket(ctx context.Context, bucketName string) (resp api.Bucket, err error) {
+	err = c.c.WithContext(ctx).GET(fmt.Sprintf("/bucket/%s", bucketName), &resp)
+	return
+}
+
 // CreateBucket creates a new bucket.
 func (c *Client) CreateBucket(ctx context.Context, bucketName string, opts api.CreateBucketOptions) error {
 	return c.c.WithContext(ctx).POST("/buckets", api.BucketCreateRequest{
@@ -15,26 +21,20 @@ func (c *Client) CreateBucket(ctx context.Context, bucketName string, opts api.C
 	}, nil)
 }
 
-// UpdateBucketPolicy updates the policy of an existing bucket.
-func (c *Client) UpdateBucketPolicy(ctx context.Context, bucketName string, policy api.BucketPolicy) error {
-	return c.c.WithContext(ctx).PUT(fmt.Sprintf("/buckets/%s/policy", bucketName), api.BucketUpdatePolicyRequest{
-		Policy: policy,
-	})
-}
-
 // DeleteBucket deletes an existing bucket. Fails if the bucket isn't empty.
 func (c *Client) DeleteBucket(ctx context.Context, bucketName string) error {
-	return c.c.WithContext(ctx).DELETE(fmt.Sprintf("/buckets/%s", bucketName))
-}
-
-// Bucket returns information about a specific bucket.
-func (c *Client) Bucket(ctx context.Context, bucketName string) (resp api.Bucket, err error) {
-	err = c.c.WithContext(ctx).GET(fmt.Sprintf("/buckets/%s", bucketName), &resp)
-	return
+	return c.c.WithContext(ctx).DELETE(fmt.Sprintf("/bucket/%s", bucketName))
 }
 
 // ListBuckets lists all available buckets.
 func (c *Client) ListBuckets(ctx context.Context) (buckets []api.Bucket, err error) {
 	err = c.c.WithContext(ctx).GET("/buckets", &buckets)
 	return
+}
+
+// UpdateBucketPolicy updates the policy of an existing bucket.
+func (c *Client) UpdateBucketPolicy(ctx context.Context, bucketName string, policy api.BucketPolicy) error {
+	return c.c.WithContext(ctx).PUT(fmt.Sprintf("/bucket/%s/policy", bucketName), api.BucketUpdatePolicyRequest{
+		Policy: policy,
+	})
 }
