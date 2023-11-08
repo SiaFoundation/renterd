47727f99043acf2af37ae9598b44ad7d2ca412e1
PJ <peterjan.brone@gmail.com>
2023-11-08 19:28:41 +0100

    testing: fix TestSQLMetadataStore

--- stores/metadata_test.go
+++ stores/metadata_test.go
@@ -1130,6 +1130,7 @@ func TestSQLMetadataStore(t *testing.T) {
 		slabs[i].Shards[0].Model = Model{}
 		slabs[i].Shards[0].Contracts[0].Model = Model{}
 		slabs[i].Shards[0].Contracts[0].Host.Model = Model{}
+		slabs[i].HealthValidUntil = 0
 	}
 	if !reflect.DeepEqual(slab1, expectedObjSlab1) {
 		t.Fatal("mismatch", cmp.Diff(slab1, expectedObjSlab1))
