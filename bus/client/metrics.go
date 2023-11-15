25222055a5ecc9db694fdfd11184ea6279bfe6e0
PJ <peterjan.brone@gmail.com>
2023-11-15 15:09:15 +0100

    bus: fix client

--- bus/client/metrics.go
+++ bus/client/metrics.go
@@ -10,17 +10,20 @@ import (
 	"go.sia.tech/renterd/api"
 )
 
-func (c *Client) ContractSetMetrics(ctx context.Context, start time.Time, n uint64, interval time.Duration, opts api.ContractSetMetricsQueryOpts) ([]api.ContractSetMetric, error) {
+func (c *Client) ContractMetrics(ctx context.Context, start time.Time, n uint64, interval time.Duration, opts api.ContractMetricsQueryOpts) ([]api.ContractMetric, error) {
 	values := url.Values{}
 	values.Set("start", api.TimeRFC3339(start).String())
 	values.Set("n", fmt.Sprint(n))
 	values.Set("interval", api.DurationMS(interval).String())
-	if opts.Name != "" {
-		values.Set("name", opts.Name)
+	if opts.ContractID != (types.FileContractID{}) {
+		values.Set("fcid", opts.ContractID.String())
 	}
-	var resp []api.ContractSetMetric
-	err := c.c.WithContext(ctx).GET(fmt.Sprintf("/metric/%s?%s", api.MetricContractSet, values.Encode()), &resp)
-	if err != nil {
+	if opts.HostKey != (types.PublicKey{}) {
+		values.Set("hostKey", opts.HostKey.String())
+	}
+
+	var resp []api.ContractMetric
+	if err := c.metric(ctx, api.MetricContract, values, &resp); err != nil {
 		return nil, err
 	}
 	return resp, nil
@@ -40,9 +43,25 @@ func (c *Client) ContractSetChurnMetrics(ctx context.Context, start time.Time, n
 	if opts.Reason != "" {
 		values.Set("reason", string(opts.Reason))
 	}
+
 	var resp []api.ContractSetChurnMetric
-	err := c.c.WithContext(ctx).GET(fmt.Sprintf("/metric/%s?%s", api.MetricContractSetChurn, values.Encode()), &resp)
-	if err != nil {
+	if err := c.metric(ctx, api.MetricContractSetChurn, values, &resp); err != nil {
+		return nil, err
+	}
+	return resp, nil
+}
+
+func (c *Client) ContractSetMetrics(ctx context.Context, start time.Time, n uint64, interval time.Duration, opts api.ContractSetMetricsQueryOpts) ([]api.ContractSetMetric, error) {
+	values := url.Values{}
+	values.Set("start", api.TimeRFC3339(start).String())
+	values.Set("n", fmt.Sprint(n))
+	values.Set("interval", api.DurationMS(interval).String())
+	if opts.Name != "" {
+		values.Set("name", opts.Name)
+	}
+
+	var resp []api.ContractSetMetric
+	if err := c.metric(ctx, api.MetricContractSet, values, &resp); err != nil {
 		return nil, err
 	}
 	return resp, nil
@@ -54,21 +73,6 @@ func (c *Client) RecordContractSetChurnMetric(ctx context.Context, metrics ...ap
 	})
 }
 
-func (c *Client) ContractMetrics(ctx context.Context, start time.Time, n uint64, interval time.Duration, opts api.ContractMetricsQueryOpts) ([]api.ContractMetric, error) {
-	values := url.Values{}
-	values.Set("start", api.TimeRFC3339(start).String())
-	values.Set("n", fmt.Sprint(n))
-	values.Set("interval", api.DurationMS(interval).String())
-	if opts.ContractID != (types.FileContractID{}) {
-		values.Set("fcid", opts.ContractID.String())
-	}
-	if opts.HostKey != (types.PublicKey{}) {
-		values.Set("hostKey", opts.HostKey.String())
-	}
-	var resp []api.ContractMetric
-	err := c.c.WithContext(ctx).GET(fmt.Sprintf("/metric/%s?%s", api.MetricContract, values.Encode()), &resp)
-	if err != nil {
-		return nil, err
-	}
-	return resp, nil
+func (c *Client) metric(ctx context.Context, key string, values url.Values, resp interface{}) error {
+	return c.c.WithContext(ctx).GET(fmt.Sprintf("/metric/%s?"+values.Encode(), key), &resp)
 }
