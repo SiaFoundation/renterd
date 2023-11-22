3be7d109cadb89091117207c07f68efab665d9f4
Chris Schinnerl <chris@sia.tech>
2023-11-22 17:00:08 +0100

    stores: fix test for MySQL

--- stores/metadata.go
+++ stores/metadata.go
@@ -1424,7 +1424,8 @@ func (s *SQLStore) RenameObject(ctx context.Context, bucket, keyOld, keyNew stri
 			}
 		}
 		tx = tx.Exec(`UPDATE objects SET object_id = ? WHERE object_id = ? AND ?`, keyNew, keyOld, sqlWhereBucket("objects", bucket))
-		if tx.Error != nil && strings.Contains(tx.Error.Error(), "UNIQUE constraint failed") {
+		if tx.Error != nil &&
+			(strings.Contains(tx.Error.Error(), "UNIQUE constraint failed") || strings.Contains(tx.Error.Error(), "Duplicate entry")) {
 			return api.ErrObjectExists
 		} else if tx.Error != nil {
 			return tx.Error
@@ -1453,7 +1454,8 @@ func (s *SQLStore) RenameObjects(ctx context.Context, bucket, prefixOld, prefixN
 		}
 		tx = tx.Exec("UPDATE objects SET object_id = "+sqlConcat(tx, "?", "SUBSTR(object_id, ?)")+" WHERE SUBSTR(object_id, 1, ?) = ?",
 			prefixNew, utf8.RuneCountInString(prefixOld)+1, utf8.RuneCountInString(prefixOld), prefixOld)
-		if tx.Error != nil && strings.Contains(tx.Error.Error(), "UNIQUE constraint failed") {
+		if tx.Error != nil &&
+			(strings.Contains(tx.Error.Error(), "UNIQUE constraint failed") || strings.Contains(tx.Error.Error(), "Duplicate entry")) {
 			return api.ErrObjectExists
 		} else if tx.Error != nil {
 			return tx.Error
