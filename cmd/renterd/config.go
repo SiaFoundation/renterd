7c52b3ae80e66bd81c8cb5262c235dbe9ebd7390
Nate Maninger <me@n8m.us>
2024-01-15 07:56:53 -0800

    cmd: add helper for question prompts

--- cmd/renterd/config.go
+++ cmd/renterd/config.go
@@ -20,22 +20,24 @@ import (
 var enableANSI = runtime.GOOS != "windows"
 
 // readPasswordInput reads a password from stdin.
-func readPasswordInput(context string) (string, error) {
+func readPasswordInput(context string) string {
 	fmt.Printf("%s: ", context)
 	input, err := term.ReadPassword(int(os.Stdin.Fd()))
-	fmt.Println()
-	return string(input), err
+	if err != nil {
+		stdoutFatalError("Could not read input: " + err.Error())
+	}
+	fmt.Println("")
+	return string(input)
 }
 
-func readInput(context string) (string, error) {
+func readInput(context string) string {
 	fmt.Printf("%s: ", context)
 	r := bufio.NewReader(os.Stdin)
 	input, err := r.ReadString('\n')
 	if err != nil {
-		return "", err
+		stdoutFatalError("Could not read input: " + err.Error())
 	}
-	fmt.Println()
-	return strings.TrimSpace(input), nil
+	return strings.TrimSpace(input)
 }
 
 // wrapANSI wraps the output in ANSI escape codes if enabled.
@@ -44,6 +46,47 @@ func wrapANSI(prefix, output, suffix string) string {
 		return prefix + output + suffix
 	}
 	return output
+}
+
+func humanList(s []string, sep string) string {
+	if len(s) == 0 {
+		return ""
+	} else if len(s) == 1 {
+		return fmt.Sprintf(`%q`, s[0])
+	} else if len(s) == 2 {
+		return fmt.Sprintf(`%q %s %q`, s[0], sep, s[1])
+	}
+
+	var sb strings.Builder
+	for i, v := range s {
+		if i != 0 {
+			sb.WriteString(", ")
+		}
+		if i == len(s)-1 {
+			sb.WriteString("or ")
+		}
+		sb.WriteString(`"`)
+		sb.WriteString(v)
+		sb.WriteString(`"`)
+	}
+	return sb.String()
+}
+
+func promptQuestion(question string, answers []string) string {
+	for {
+		input := readInput(fmt.Sprintf("%s (%s)", question, strings.Join(answers, "/")))
+		for _, answer := range answers {
+			if strings.EqualFold(input, answer) {
+				return answer
+			}
+		}
+		fmt.Println(wrapANSI("\033[31m", fmt.Sprintf("Answer must be %s", humanList(answers, "or")), "\033[0m"))
+	}
+}
+
+func promptYesNo(question string) bool {
+	answer := promptQuestion(question, []string{"yes", "no"})
+	return strings.EqualFold(answer, "yes")
 }
 
 // stdoutFatalError prints an error message to stdout and exits with a 1 exit code.
@@ -64,12 +107,7 @@ func stdoutError(msg string) {
 func setListenAddress(context string, value *string) {
 	// will continue to prompt until a valid value is entered
 	for {
-		input, err := readInput(fmt.Sprintf("%s (currently %q)", context, *value))
-		if err != nil {
-			stdoutFatalError(fmt.Sprintf("Could not read %s port: %s", context, err.Error()))
-			return
-		}
-
+		input := readInput(fmt.Sprintf("%s (currently %q)", context, *value))
 		if input == "" {
 			return
 		}
@@ -100,10 +138,7 @@ func setSeedPhrase() {
 	for {
 		fmt.Println("")
 		fmt.Println("Type in your 12-word seed phrase and press enter. If you do not have a seed phrase yet, type 'seed' to generate one.")
-		phrase, err := readPasswordInput("Enter seed phrase")
-		if err != nil {
-			stdoutFatalError("Could not read seed phrase: " + err.Error())
-		}
+		phrase := readPasswordInput("Enter seed phrase")
 
 		if strings.ToLower(strings.TrimSpace(phrase)) == "seed" {
 			// generate a new seed phrase
@@ -113,6 +148,7 @@ func setSeedPhrase() {
 				panic(err)
 			}
 			key := wallet.KeyFromSeed(&seed, 0)
+			fmt.Println("")
 			fmt.Println("")
 			fmt.Println("A new seed phrase has been generated below. " + wrapANSI("\033[1m", "Write it down and keep it safe.", "\033[0m"))
 			fmt.Println("Your seed phrase is the only way to recover your Siacoin. If you lose your seed phrase, you will also lose your Siacoin.")
@@ -124,10 +160,9 @@ func setSeedPhrase() {
 			for {
 				fmt.Println("")
 				fmt.Println(wrapANSI("\033[1m", "Please confirm your seed phrase to continue.", "\033[0m"))
-				confirmPhrase, err := readPasswordInput("Enter seed phrase")
-				if err != nil {
-					stdoutFatalError("Could not read seed phrase: " + err.Error())
-				} else if confirmPhrase == phrase {
+
+				confirmPhrase := readPasswordInput("Enter seed phrase")
+				if confirmPhrase == phrase {
 					cfg.Seed = phrase
 					return
 				}
@@ -139,14 +174,16 @@ func setSeedPhrase() {
 		}
 
 		var seed [32]byte
-		err = wallet.SeedFromPhrase(&seed, phrase)
-		if err == nil {
-			// valid seed phrase
-			cfg.Seed = phrase
-			return
+		if err := wallet.SeedFromPhrase(&seed, phrase); err != nil {
+			// invalid seed phrase, retry
+			fmt.Println(wrapANSI("\033[31m", "Invalid seed phrase:", "\033[0m"), err)
+			fmt.Println("You entered:", phrase)
+			continue
 		}
-		fmt.Println(wrapANSI("\033[31m", "Invalid seed phrase:", "\033[0m"), err)
-		fmt.Println("You entered:", phrase)
+
+		// valid seed phrase
+		cfg.Seed = phrase
+		return
 	}
 }
 
@@ -158,35 +195,29 @@ func setAPIPassword() {
 		fmt.Println("Please choose a password for the renterd admin UI.")
 		fmt.Println("This password will be required to access the admin UI in your web browser.")
 		fmt.Println("(The password must be at least 4 characters.)")
-		var err error
-		cfg.HTTP.Password, err = readPasswordInput("Enter password")
-		if err != nil {
-			stdoutFatalError("Could not read password:" + err.Error())
-		}
 
+		cfg.HTTP.Password = readPasswordInput("Enter password")
 		if len(cfg.HTTP.Password) >= 4 {
 			break
 		}
 
+		// invalid password, retry
 		fmt.Println(wrapANSI("\033[31m", "Password must be at least 4 characters!", "\033[0m"))
 		fmt.Println("")
 	}
 }
 
 func setAdvancedConfig() {
-	input, err := readInput("Would you like to configure advanced settings? (yes/no)")
-	if err != nil {
-		stdoutFatalError("Could not read input: " + err.Error())
-	} else if !strings.EqualFold(input, "yes") {
+	if !promptYesNo("Would you like to configure advanced settings?") {
 		return
 	}
 
 	fmt.Println("")
 	fmt.Println("Advanced settings are used to configure the renter's behavior.")
 	fmt.Println("You can leave these settings blank to use the defaults.")
-	fmt.Println("")
 
 	// http address
+	fmt.Println("")
 	fmt.Println("The HTTP address is used to serve the renter's admin API.")
 	fmt.Println("The admin API is used to configure the renter.")
 	fmt.Println("It should not be exposed to the public internet without setting up a reverse proxy.")
@@ -200,10 +231,7 @@ func setAdvancedConfig() {
 }
 
 func setS3Config() {
-	input, err := readInput("Would you like to configure S3 settings? (yes/no)")
-	if err != nil {
-		stdoutFatalError("Could not read input: " + err.Error())
-	} else if !strings.EqualFold(input, "yes") {
+	if !promptYesNo("Would you like to configure S3 settings?") {
 		return
 	}
 
@@ -223,10 +251,7 @@ func setS3Config() {
 		fmt.Println("")
 		fmt.Println("A S3 keypair has already been created.")
 		fmt.Println("If you change your S3 key pair, you will need to update any scripts or applications that use the S3 API.")
-		input, err := readInput("Would you like to change your S3 key pair? (yes/no)")
-		if err != nil {
-			stdoutFatalError("Could not read input: " + err.Error())
-		} else if strings.EqualFold(input, "no") {
+		if !promptYesNo("Would you like to change your S3 key pair?") {
 			return
 		}
 	}
@@ -234,11 +259,8 @@ func setS3Config() {
 	cfg.S3.KeypairsV4 = make(map[string]string)
 
 	fmt.Println("")
-	fmt.Println("Would you like to automatically generate a new S3 key pair or enter your own? (auto/manual)")
-	input, err = readInput("Enter auto or manual")
-	if err != nil {
-		stdoutFatalError("Could not read input: " + err.Error())
-	} else if strings.EqualFold(input, "auto") {
+	answer := promptQuestion("Would you like to automatically generate a new S3 key pair or set your own?", []string{"auto", "manual"})
+	if strings.EqualFold(answer, "auto") {
 		// generate a new key pair
 		accessKey := hex.EncodeToString(frand.Bytes(20))
 		secretKey := hex.EncodeToString(frand.Bytes(20))
@@ -255,37 +277,29 @@ func setS3Config() {
 	for {
 		fmt.Println("")
 		fmt.Println("Enter your S3 access key. It must between 16 and 128 characters long.")
-		accessKey, err = readInput("Enter access key")
-		if err != nil {
-			stdoutFatalError("Could not read access key: " + err.Error())
-		} else if len(accessKey) >= 16 && len(accessKey) <= 128 {
+		accessKey = readInput("Enter access key")
+		if len(accessKey) >= 16 && len(accessKey) <= 128 {
 			break
 		}
-
 		fmt.Println(wrapANSI("\033[31m", "Access key must be between 16 and 128 characters!", "\033[0m"))
 	}
 
 	for {
 		fmt.Println("")
 		fmt.Println("Enter your S3 secret key. It must be 40 characters long.")
-		secretKey, err = readInput("Enter secret key")
-		if err != nil {
-			stdoutFatalError("Could not read secret key: " + err.Error())
-		} else if len(secretKey) != 40 {
+		secretKey = readInput("Enter secret key")
+		if len(secretKey) == 40 {
 			break
 		}
-
 		fmt.Println(wrapANSI("\033[31m", "Secret key must be be 40 characters!", "\033[0m"))
 	}
+
+	cfg.S3.KeypairsV4[accessKey] = secretKey
 }
 
 func cmdBuildConfig() {
 	if _, err := os.Stat("renterd.yml"); err == nil {
-		input, err := readInput("renterd.yml already exists. Would you like to overwrite it? (yes/no)")
-		if err != nil {
-			stdoutFatalError("Could not read input: " + err.Error())
-			return
-		} else if !strings.EqualFold(input, "yes") {
+		if !promptYesNo("renterd.yml already exists. Would you like to overwrite it?") {
 			return
 		}
 	}
@@ -295,10 +309,7 @@ func cmdBuildConfig() {
 		fmt.Println(wrapANSI("\033[33m", "A wallet seed phrase is already set.", "\033[0m"))
 		fmt.Println("If you change your wallet seed phrase, your renter will not be able to access Siacoin associated with this wallet.")
 		fmt.Println("Ensure that you have backed up your wallet seed phrase before continuing.")
-		input, err := readInput("Would you like to change your wallet seed phrase? (yes/no)")
-		if err != nil {
-			stdoutFatalError("Could not read input: " + err.Error())
-		} else if strings.EqualFold(input, "yes") {
+		if promptYesNo("Would you like to change your wallet seed phrase?") {
 			setSeedPhrase()
 			fmt.Println("")
 		}
@@ -311,10 +322,7 @@ func cmdBuildConfig() {
 		fmt.Println("")
 		fmt.Println(wrapANSI("\033[33m", "An admin password is already set.", "\033[0m"))
 		fmt.Println("If you change your admin password, you will need to update any scripts or applications that use the admin API.")
-		input, err := readInput("Would you like to change your admin password? (yes/no)")
-		if err != nil {
-			stdoutFatalError("Could not read input: " + err.Error())
-		} else if strings.EqualFold(input, "yes") {
+		if promptYesNo("Would you like to change your admin password?") {
 			setAPIPassword()
 			fmt.Println("")
 		}
@@ -324,6 +332,7 @@ func cmdBuildConfig() {
 	}
 
 	setS3Config()
+	fmt.Println("")
 	setAdvancedConfig()
 
 	// write the config file
