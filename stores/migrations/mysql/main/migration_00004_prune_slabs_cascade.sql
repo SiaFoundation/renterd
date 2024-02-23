99014c79fe65c3bffa5c737a768d3905351c7517
Chris Schinnerl <chris@sia.tech>
2024-02-23 09:47:48 +0100

    stores: drop indices first

--- stores/migrations/mysql/main/migration_00004_prune_slabs_cascade.sql
+++ stores/migrations/mysql/main/migration_00004_prune_slabs_cascade.sql
@@ -1,3 +1,9 @@
+-- drop triggers
+DROP TRIGGER IF EXISTS before_delete_on_objects_delete_slices;
+DROP TRIGGER IF EXISTS before_delete_on_multipart_uploads_delete_multipart_parts;
+DROP TRIGGER IF EXISTS before_delete_on_multipart_parts_delete_slices;
+DROP TRIGGER IF EXISTS after_delete_on_slices_delete_slabs;
+
 -- add ON DELETE CASCADE to slices
 ALTER TABLE slices DROP FOREIGN KEY fk_objects_slabs;
 ALTER TABLE slices ADD CONSTRAINT fk_objects_slabs FOREIGN KEY (db_object_id) REFERENCES objects (id) ON DELETE CASCADE;
@@ -8,9 +14,3 @@ ALTER TABLE slices ADD CONSTRAINT fk_multipart_parts_slabs FOREIGN KEY (db_multi
 -- add ON DELETE CASCADE to multipart_parts
 ALTER TABLE multipart_parts DROP FOREIGN KEY fk_multipart_uploads_parts;
 ALTER TABLE multipart_parts ADD CONSTRAINT fk_multipart_uploads_parts FOREIGN KEY (db_multipart_upload_id) REFERENCES multipart_uploads (id) ON DELETE CASCADE;
-
--- drop triggers
-DROP TRIGGER IF EXISTS before_delete_on_objects_delete_slices;
-DROP TRIGGER IF EXISTS before_delete_on_multipart_uploads_delete_multipart_parts;
-DROP TRIGGER IF EXISTS before_delete_on_multipart_parts_delete_slices;
-DROP TRIGGER IF EXISTS after_delete_on_slices_delete_slabs;
