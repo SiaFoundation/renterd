e09f7ca24cab20773b64f95c7b4f57324b5cbe19
PJ <peterjan.brone@gmail.com>
2024-02-29 10:22:49 +0100

    worker: fix TestUploadPackedSlab NDF

--- internal/test/tt.go
+++ internal/test/tt.go
@@ -90,12 +90,16 @@ func (t impl) OKAll(vs ...interface{}) {
 
 func (t impl) Retry(tries int, durationBetweenAttempts time.Duration, fn func() error) {
 	t.Helper()
-	for i := 1; i < tries; i++ {
-		err := fn()
+	t.OK(Retry(tries, durationBetweenAttempts, fn))
+}
+
+func Retry(tries int, durationBetweenAttempts time.Duration, fn func() error) (err error) {
+	for i := 0; i < tries; i++ {
+		err = fn()
 		if err == nil {
-			return
+			return nil
 		}
 		time.Sleep(durationBetweenAttempts)
 	}
-	t.OK(fn())
+	return
 }
