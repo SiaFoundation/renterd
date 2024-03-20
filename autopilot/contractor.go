864c08b7b12f5426b1775a3e7d0520deb3cec6dc
PJ <peterjan.brone@gmail.com>
2024-03-20 15:02:36 +0100

    contractor: fetch all hosts

--- autopilot/contractor.go
+++ autopilot/contractor.go
@@ -240,7 +240,7 @@ func (c *contractor) performContractMaintenance(ctx context.Context, w Worker) (
 	}
 
 	// fetch all hosts
-	hosts, err := c.ap.bus.SearchHosts(ctx, api.SearchHostOptions{Limit: -1, FilterMode: api.HostFilterModeAllowed})
+	hosts, err := c.ap.bus.SearchHosts(ctx, api.SearchHostOptions{Limit: -1, FilterMode: api.HostFilterModeAll})
 	if err != nil {
 		return false, err
 	}
