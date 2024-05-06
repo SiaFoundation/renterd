67f76448e16afcfcce169cae8b6f26f5d1f3f8a5
Chris Schinnerl <chris@sia.tech>
2024-05-06 15:32:16 +0200

    autopilot: update current period

--- autopilot/autopilot.go
+++ autopilot/autopilot.go
@@ -876,14 +876,20 @@ func (ap *Autopilot) buildState(ctx context.Context) (*contractor.MaintenanceSta
 				return nil, err
 			}
 			ap.logger.Infof("initialised current period to %d", autopilot.CurrentPeriod)
-		} else if nextPeriod := autopilot.CurrentPeriod + autopilot.Config.Contracts.Period; cs.BlockHeight >= nextPeriod {
+		} else {
 			prevPeriod := autopilot.CurrentPeriod
-			autopilot.CurrentPeriod = nextPeriod
-			err := ap.bus.UpdateAutopilot(ctx, autopilot)
-			if err != nil {
-				return nil, err
+			nextPeriod := prevPeriod
+			for cs.BlockHeight >= nextPeriod+autopilot.Config.Contracts.Period {
+				nextPeriod += autopilot.Config.Contracts.Period
 			}
-			ap.logger.Infof("updated current period from %d to %d", prevPeriod, nextPeriod)
+			if nextPeriod > prevPeriod {
+				autopilot.CurrentPeriod = nextPeriod
+				err := ap.bus.UpdateAutopilot(ctx, autopilot)
+				if err != nil {
+					return nil, err
+				}
+				ap.logger.Infof("updated current period from %d to %d", prevPeriod, nextPeriod)
+			}
 		}
 	}
 
