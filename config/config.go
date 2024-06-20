da760ba66cb5967d031e6b9338dba96d733787a7
PJ <peterjan.brone@gmail.com>
2024-06-20 14:48:38 +0200

    node: add worker.networkAddress

--- config/config.go
+++ config/config.go
@@ -124,6 +124,7 @@ type (
 		UploadMaxMemory               uint64         `yaml:"uploadMaxMemory,omitempty"`
 		UploadMaxOverdrive            uint64         `yaml:"uploadMaxOverdrive,omitempty"`
 		AllowUnauthenticatedDownloads bool           `yaml:"allowUnauthenticatedDownloads,omitempty"`
+		NetworkAddress                string         `yaml:"networkAddress,omitempty"`
 	}
 
 	// Autopilot contains the configuration for an autopilot.
