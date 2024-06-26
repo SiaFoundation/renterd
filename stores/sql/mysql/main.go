f3e7cff07dfff78abb143ec61426ee702e0209e0
Chris Schinnerl <chris@sia.tech>
2024-06-26 16:15:50 +0200

    mysql: update UpdateSlabHealth

--- stores/sql/mysql/main.go
+++ stores/sql/mysql/main.go
@@ -953,12 +953,18 @@ func (tx *MainDatabaseTx) UpdateSlabHealth(ctx context.Context, limit int64, min
 	_, err = tx.Exec(ctx, `
 		UPDATE objects o
 		INNER JOIN (
-			SELECT sli.db_object_id as id, MIN(h.health) as health
-			FROM slabs_health h
-			INNER JOIN slices sli ON sli.db_slab_id = h.id
+			SELECT sli.db_object_id as id, MIN(sla.health) as health
+			FROM slabs sla
+			INNER JOIN slices sli ON sli.db_slab_id = sla.id
 			GROUP BY sli.db_object_id
 		) AS object_health ON object_health.id = o.id
 		SET o.health = object_health.health
+		WHERE EXISTS (
+			SELECT 1
+			FROM slabs_health h
+			INNER JOIN slices ON slices.db_slab_id = h.id
+			WHERE slices.db_object_id = o.id
+		)
 		`)
 	if err != nil {
 		return 0, fmt.Errorf("failed to update object health: %w", err)
