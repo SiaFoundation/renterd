2345b243cffffab4d66472728fe89cc8a49a4059
PJ <peterjan.brone@gmail.com>
2024-07-18 11:43:45 +0200

    stores: dont error out if we update the autopilot with the same config

--- stores/sql/sqlite/main.go
+++ stores/sql/sqlite/main.go
@@ -784,21 +784,14 @@ func (tx *MainDatabaseTx) UnspentSiacoinElements(ctx context.Context) (elements 
 }
 
 func (tx *MainDatabaseTx) UpdateAutopilot(ctx context.Context, ap api.Autopilot) error {
-	res, err := tx.Exec(ctx, `
+	_, err := tx.Exec(ctx, `
 		INSERT INTO autopilots (created_at, identifier, config, current_period)
 		VALUES (?, ?, ?, ?)
 		ON CONFLICT(identifier) DO UPDATE SET
 		config = EXCLUDED.config,
 		current_period = EXCLUDED.current_period
 	`, time.Now(), ap.ID, (*ssql.AutopilotConfig)(&ap.Config), ap.CurrentPeriod)
-	if err != nil {
-		return err
-	} else if n, err := res.RowsAffected(); err != nil {
-		return err
-	} else if n != 1 {
-		return fmt.Errorf("expected 1 row affected, got %v", n)
-	}
-	return nil
+	return err
 }
 
 func (tx *MainDatabaseTx) UpdateBucketPolicy(ctx context.Context, bucket string, policy api.BucketPolicy) error {
