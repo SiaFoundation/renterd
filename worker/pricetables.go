67cf2996f02e368a28734028bbaf91a38c17fb35
Chris Schinnerl <chris@sia.tech>
2024-07-29 10:43:18 +0200

    worker: address review comments

--- worker/pricetables.go
+++ worker/pricetables.go
@@ -193,3 +193,16 @@ func (p *priceTable) fetch(ctx context.Context, rev *types.FileContractRevision)
 	}
 	return
 }
+
+func shouldRecordPriceTable(err error) bool {
+	// List of errors that are considered 'successful' failures. Meaning that
+	// the host was reachable but we were unable to obtain a price table due to
+	// reasons out of the host's control.
+	if isInsufficientFunds(err) {
+		return false
+	}
+	if isBalanceInsufficient(err) {
+		return false
+	}
+	return true
+}
