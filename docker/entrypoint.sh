c5494ace595a1fd11ef8b487d5ba573a8d60ed3f
Chris Schinnerl <chris@sia.tech>
2024-08-08 15:27:34 +0200

    docker: update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-if [[ "$BUILD_TAGS" == *'testnet'* ]]; then
-    exec renterd -env -http=':9880' -s3.address=':7070' "$@"
-else
+if [[ "$RENTERD_NETWORK" == *'mainnet'* ]]; then
     exec renterd -env -http=':9980' -s3.address=':8080' "$@"
+else
+    exec renterd -env -http=':9880' -s3.address=':7070' "$@"
 fi
