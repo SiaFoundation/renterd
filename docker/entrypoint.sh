049ddb5552ebe1c554f6176312d37f1d1b4a0839
Chris Schinnerl <chris@sia.tech>
2024-08-08 17:50:22 +0200

    remove testnet container

--- docker/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if [[ "$RENTERD_NETWORK" == *'mainnet'* ]]; then
-    exec renterd -env -http=':9980' -s3.address=':8080' "$@"
-else
-    exec renterd -env -http=':9880' -s3.address=':7070' "$@"
-fi
