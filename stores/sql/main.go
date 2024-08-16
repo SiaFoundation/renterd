14fdf9053b46b5f6522dc2c007ce6ac2a3c69e5c
Chris Schinnerl <chris@sia.tech>
2024-08-16 09:58:50 +0200

    stores: use join instead of subquery

--- stores/sql/main.go
+++ stores/sql/main.go
@@ -1458,8 +1458,9 @@ func ObjectEntries(ctx context.Context, tx Tx, bucket, path, prefix, sortBy, sor
 		FROM (
 			SELECT o.object_id, o.size, o.health, o.mime_type, o.created_at, o.etag
 			FROM objects o
+			LEFT JOIN directories d ON d.name = o.object_id
 			WHERE o.object_id != ? AND o.db_directory_id = ? AND o.db_bucket_id = (SELECT id FROM buckets b WHERE b.name = ?) %s
-				AND NOT EXISTS (SELECT 1 FROM directories d WHERE d.name = o.object_id)
+				AND d.id IS NULL
 			UNION ALL
 			SELECT d.name as object_id, SUM(o.size), MIN(o.health), '' as mime_type, MAX(o.created_at) as created_at, '' as etag
 			FROM objects o
