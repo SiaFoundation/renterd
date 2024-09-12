0a2986653b518b5aca1af64d8b52e09d4f0ad722
Chris Schinnerl <chris@sia.tech>
2024-09-12 15:31:28 +0200

    Revert "Revert "stores: remove sorting""
    This reverts commit b1ab76cd9b634c26d98943605dab477b6cffa46f.

--- bus/routes.go
+++ bus/routes.go
@@ -487,20 +487,6 @@ func (b *Bus) hostsHandlerPOST(jc jape.Context) {
 		return
 	}
 
-	// validate sorting params
-	if req.SortBy != "" {
-		if !api.IsValidHostSortBy(req.SortBy) {
-			jc.Error(fmt.Errorf("%w: %v", api.ErrInvalidHostSortBy, req.SortBy), http.StatusBadRequest)
-			return
-		}
-	}
-	switch req.SortDir {
-	case "", api.SortDirAsc, api.SortDirDesc:
-	default:
-		jc.Error(errors.New("invalid value for SortDir param, options are 'asc' and 'desc'"), http.StatusBadRequest)
-		return
-	}
-
 	// validate the offset and limit
 	if req.Offset < 0 {
 		jc.Error(errors.New("offset must be non-negative"), http.StatusBadRequest)
@@ -521,8 +507,6 @@ func (b *Bus) hostsHandlerPOST(jc jape.Context) {
 		KeyIn:           req.KeyIn,
 		Offset:          req.Offset,
 		Limit:           req.Limit,
-		SortBy:          req.SortBy,
-		SortDir:         req.SortDir,
 	})
 	if jc.Check(fmt.Sprintf("couldn't fetch hosts %d-%d", req.Offset, req.Offset+req.Limit), err) != nil {
 		return
