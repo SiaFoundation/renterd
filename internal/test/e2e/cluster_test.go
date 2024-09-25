e830df75003169debfdcc980711da5dd999ad2b2
PJ <peterjan.brone@gmail.com>
2024-09-25 14:04:46 +0200

    testing: remove debug line

--- internal/test/e2e/cluster_test.go
+++ internal/test/e2e/cluster_test.go
@@ -781,8 +781,6 @@ func TestUploadDownloadBasic(t *testing.T) {
 		}
 		return nil
 	})
-
-	w.HeadObject(context.Background(), testBucket, path, api.HeadObjectOptions{})
 }
 
 // TestUploadDownloadExtended is an integration test that verifies objects can
