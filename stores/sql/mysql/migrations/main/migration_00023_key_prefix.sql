fa5d12c7e2e527a9cc1d07b00ee570e420018222
Chris Schinnerl <chris@sia.tech>
2024-09-26 16:45:23 +0200

    sql: fix mysql migration

--- stores/sql/mysql/migrations/main/migration_00023_key_prefix.sql
+++ stores/sql/mysql/migrations/main/migration_00023_key_prefix.sql
@@ -2,6 +2,6 @@ ALTER TABLE `objects` MODIFY `key` binary(33) NOT NULL;
 ALTER TABLE `slabs` MODIFY `key` binary(33) NOT NULL;
 
 UPDATE `objects`
-SET `key` = CONCAT(x'01', `key`);
+SET `key` = CONCAT(x'01', LEFT(`key`, 32));
 UPDATE `slabs`
-SET `key` = CONCAT(x'01', `key`);
+SET `key` = CONCAT(x'01', LEFT(`key`, 32));
