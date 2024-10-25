f9311a4035b1162a637fb2e5029533bbd9b28bd2
Chris Schinnerl <chris@sia.tech>
2024-10-25 15:18:22 +0200

    e2e: fix TestWalletRedistribute

--- internal/test/e2e/cluster.go
+++ internal/test/e2e/cluster.go
@@ -489,7 +489,7 @@ func newTestCluster(t *testing.T, opts testClusterOptions) *TestCluster {
 
 	// Fund the bus.
 	if funding {
-		cluster.MineBlocks(network.HardforkFoundation.Height + blocksPerDay) // mine until the first block reward matures
+		cluster.MineBlocks(network.HardforkFoundation.Height + network.MaturityDelay) // mine until the first block reward matures
 		tt.Retry(100, 100*time.Millisecond, func() error {
 			if cs, err := busClient.ConsensusState(ctx); err != nil {
 				return err
