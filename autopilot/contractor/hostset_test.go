a489d3643e21da357cd5724e28a2b4305bd4ae49
Chris Schinnerl <chris@sia.tech>
2024-11-19 12:17:14 +0100

    utils: split up resolving addresses and checking them

--- autopilot/contractor/hostset_test.go
+++ autopilot/contractor/hostset_test.go
@@ -1,6 +1,7 @@
 package contractor
 
 import (
+	"context"
 	"testing"
 
 	"go.sia.tech/core/types"
@@ -17,8 +18,9 @@ func TestHostSet(t *testing.T) {
 		NetAddress:        "",
 		V2SiamuxAddresses: []string{},
 	}
-	if hs.HasRedundantIP(host1) {
-		t.Fatalf("Expected host with no subnets to not be redundant")
+	ctx := context.Background()
+	if !hs.HasRedundantIP(ctx, host1) {
+		t.Fatalf("Expected host with no subnets to be redundant")
 	}
 
 	// Host with more than 2 subnets
@@ -27,7 +29,7 @@ func TestHostSet(t *testing.T) {
 		NetAddress:        "1.1.1.1:1111",
 		V2SiamuxAddresses: []string{"2.2.2.2:2222", "3.3.3.3:3333"},
 	}
-	if !hs.HasRedundantIP(host2) {
+	if !hs.HasRedundantIP(ctx, host2) {
 		t.Fatalf("Expected host with more than 2 subnets to be considered redundant")
 	}
 
@@ -37,10 +39,10 @@ func TestHostSet(t *testing.T) {
 		NetAddress:        "",
 		V2SiamuxAddresses: []string{"4.4.4.4:4444"},
 	}
-	if hs.HasRedundantIP(host3) {
+	if hs.HasRedundantIP(ctx, host3) {
 		t.Fatal("Expected new host with unique subnet to not be considered redundant")
 	}
-	hs.Add(host3)
+	hs.Add(ctx, host3)
 
 	// New host with same subnet but different public key
 	host4 := api.Host{
@@ -48,12 +50,12 @@ func TestHostSet(t *testing.T) {
 		NetAddress:        "",
 		V2SiamuxAddresses: []string{"4.4.4.4:4444"},
 	}
-	if !hs.HasRedundantIP(host4) {
+	if !hs.HasRedundantIP(ctx, host4) {
 		t.Fatal("Expected host with same subnet but different public key to be considered redundant")
 	}
 
 	// Same host from before
-	if hs.HasRedundantIP(host3) {
+	if hs.HasRedundantIP(ctx, host3) {
 		t.Fatal("Expected same host to not be considered redundant")
 	}
 
@@ -63,10 +65,10 @@ func TestHostSet(t *testing.T) {
 		NetAddress:        "",
 		V2SiamuxAddresses: []string{"5.5.5.5:5555", "[2001:0db8:85a3:0000:0000:8a2e:0370:7334]:5555"},
 	}
-	if hs.HasRedundantIP(host5) {
+	if hs.HasRedundantIP(ctx, host5) {
 		t.Fatal("Expected host with two valid subnets to not be considered redundant")
 	}
-	hs.Add(host5)
+	hs.Add(ctx, host5)
 
 	// New host with one overlapping subnet
 	host6 := api.Host{
@@ -74,7 +76,7 @@ func TestHostSet(t *testing.T) {
 		NetAddress:        "",
 		V2SiamuxAddresses: []string{"6.6.6.6:6666", "7.7.7.7:7777"},
 	}
-	if !hs.HasRedundantIP(host6) {
+	if !hs.HasRedundantIP(ctx, host6) {
 		t.Fatal("Expected host with one overlapping subnet to be considered redundant")
 	}
 	host7 := api.Host{
@@ -82,7 +84,7 @@ func TestHostSet(t *testing.T) {
 		NetAddress:        "6.6.6.6:6666",
 		V2SiamuxAddresses: []string{"8.8.8.8:8888"},
 	}
-	if !hs.HasRedundantIP(host7) {
+	if !hs.HasRedundantIP(ctx, host7) {
 		t.Fatal("Expected host with one overlapping subnet to be considered redundant")
 	}
 }
