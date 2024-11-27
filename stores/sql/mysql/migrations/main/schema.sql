d578e8660cc02c4dae7e5886317d7bc9a5b93434
Chris Schinnerl <chris@sia.tech>
2024-11-27 12:04:51 +0100

    sql: extend mysql schema

--- stores/sql/mysql/migrations/main/schema.sql
+++ stores/sql/mysql/migrations/main/schema.sql
@@ -479,3 +479,16 @@ CREATE TABLE `wallet_outputs` (
   UNIQUE KEY `output_id` (`output_id`),
   KEY `idx_wallet_outputs_maturity_height` (`maturity_height`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- contract elements
+CREATE TABLE `contract_elements` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `created_at` datetime(3) DEFAULT NULL,
+  `db_contract_id` bigint unsigned NOT NULL,
+  `contract` longblob NOT NULL,
+  `leaf_index` bigint,
+  `merkle_proof` longblob NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idx_contract_elements_db_contrat_id` (`db_contract_id`),
+  CONSTRAINT `fk_contract_elements_contracts` FOREIGN KEY (`db_contract_id`) REFERENCES `contracts` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
