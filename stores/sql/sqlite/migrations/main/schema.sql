0db7f6ad807aee23601ece5208d4f5128239f74a
Chris Schinnerl <chris@sia.tech>
2024-11-27 12:04:51 +0100

    sql: extend mysql schema

--- stores/sql/sqlite/migrations/main/schema.sql
+++ stores/sql/sqlite/migrations/main/schema.sql
@@ -176,8 +176,9 @@ CREATE INDEX `idx_wallet_outputs_maturity_height` ON `wallet_outputs`(`maturity_
 CREATE TABLE `contract_elements` (
     `id` integer PRIMARY KEY AUTOINCREMENT,
     `created_at` datetime,
-    `db_contract_id` integer NOT NULL REFERENCES `contracts`(`id`) ON DELETE CASCADE,
-    `contract` blob,
+    `db_contract_id` integer NOT NULL,
+    `contract` blob NOT NULL,
     `leaf_index` integer,
-    `merkle_proof` longblob NOT NULL);
+    `merkle_proof` longblob NOT NULL,
+    CONSTRAINT `fk_contract_elements_contracts` FOREIGN KEY (`db_contract_id`) REFERENCES `contracts`(`id`) ON DELETE CASCADE);
 CREATE UNIQUE INDEX `idx_contract_elements_db_contract_id` ON `contract_elements`(`db_contract_id`);
