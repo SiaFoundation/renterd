fba07f8cfbb224a38b89ba1a7a23763a5cf1bd1c
Chris Schinnerl <chris@sia.tech>
2024-12-18 11:54:21 +0100

    chore(test): update HardforkV2Require Height for v2 tests to 3

--- internal/test/e2e/cluster_test.go
+++ internal/test/e2e/cluster_test.go
@@ -2932,7 +2932,7 @@ func TestV1ToV2Transition(t *testing.T) {
 	apCfg := test.AutopilotConfig
 	apCfg.Contracts.Amount = 2
 	apCfg.Contracts.Period = 1000 // make sure we handle trying to form contracts with a proof height after the v2 require height
-	apCfg.Contracts.RenewWindow = 50
+	apCfg.Contracts.RenewWindow = 1
 
 	// create a test cluster
 	nHosts := 3
