afbdf006e1e56e14066aa028b06088eb0ba94edb
Chris Schinnerl <chris@sia.tech>
2025-01-23 11:32:24 +0100

    reduce syncer timeouts in testing

--- internal/test/e2e/cluster.go
+++ internal/test/e2e/cluster.go
@@ -613,7 +613,10 @@ func newTestBus(ctx context.Context, cm *chain.Manager, genesisBlock types.Block
 	}
 
 	// create the syncer
-	s := syncer.New(l, cm, sqlStore, header, syncer.WithLogger(logger.Named("syncer")), syncer.WithSendBlocksTimeout(time.Minute))
+	s := syncer.New(l, cm, sqlStore, header, syncer.WithLogger(logger.Named("syncer")),
+		syncer.WithSendBlocksTimeout(2*time.Second),
+		syncer.WithRPCTimeout(2*time.Second),
+	)
 
 	// start syncer
 	errChan := make(chan error, 1)
