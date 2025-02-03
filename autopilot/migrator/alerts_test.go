d3d2e2d59cd845934acebdd5f5b376efad00717c
PJ <peterjan.brone@gmail.com>
2025-02-03 13:30:47 +0100

    testing: add TestFilterMigrationFailedAlertIDs

---
+++ autopilot/migrator/alerts_test.go
@@ -0,0 +1,37 @@
+package migrator
+
+import (
+	"testing"
+
+	"go.sia.tech/core/types"
+	"go.sia.tech/renterd/alerts"
+	"go.sia.tech/renterd/object"
+)
+
+func TestFilterMigrationFailedAlertIDs(t *testing.T) {
+	sk := object.GenerateEncryptionKey(object.EncryptionKeyTypeSalted)
+	if ids := filterMigrationFailedAlertIDs([]alerts.Alert{
+		{
+			ID:       types.Hash256{1},
+			Severity: alerts.SeverityCritical,
+			Message:  "Contract renewal failed",
+			Data:     map[string]any{"contractID": "775262e96fe67ff4c7de6d78da6d180cd7e37c584c34c0813153286ff63aae40"},
+		},
+		{
+			ID:       alerts.IDForSlab(alertMigrationID, sk),
+			Severity: alerts.SeverityCritical,
+			Message:  "Slab migration failed",
+			Data:     map[string]any{"slabKey": sk.String()},
+		},
+		{
+			ID:       types.Hash256{2},
+			Severity: alerts.SeverityInfo,
+			Message:  "Contract usability updated",
+			Data:     map[string]any{},
+		},
+	}); len(ids) != 1 {
+		t.Fatal("unexpected number of alerts", len(ids))
+	} else if ids[0] != alerts.IDForSlab(alertMigrationID, sk) {
+		t.Fatal("unexpected alert ID")
+	}
+}
