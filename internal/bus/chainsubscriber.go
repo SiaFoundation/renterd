507f7ca1e3f167b5c9865678041f56a66adf60a5
PJ <peterjan.brone@gmail.com>
2025-06-23 10:13:05 +0200

    bus: reset chain state when block is missing instead of on startup

--- internal/bus/chainsubscriber.go
+++ internal/bus/chainsubscriber.go
@@ -55,6 +55,7 @@ type (
 	ChainStore interface {
 		ChainIndex(ctx context.Context) (types.ChainIndex, error)
 		ProcessChainUpdate(ctx context.Context, applyFn func(sql.ChainUpdateTx) error) error
+		ResetChainState(ctx context.Context) error
 	}
 
 	Wallet interface {
@@ -326,7 +327,17 @@ func (s *chainSubscriber) sync() error {
 		// fetch updates
 		istart := time.Now()
 		crus, caus, err := s.cm.UpdatesSince(index, updatesBatchSize)
-		if err != nil {
+		if errors.Is(err, chain.ErrMissingBlock) {
+			s.logger.Warnf("missing block, resetting chain state", "height", index.Height, "block_id", index.ID)
+			if err := s.cs.ResetChainState(s.shutdownCtx); err != nil {
+				s.logger.Debugw("failed to reset chain state after missing block", zap.Error(err))
+				return fmt.Errorf("failed to reset chain state after missing block: %w", err)
+			} else if index, err = s.cs.ChainIndex(s.shutdownCtx); err != nil {
+				s.logger.Debugw("failed to get chain index after reset", zap.Error(err))
+				return fmt.Errorf("failed to get chain index after reset: %w", err)
+			}
+			continue
+		} else if err != nil {
 			return fmt.Errorf("failed to fetch updates: %w", err)
 		} else if len(crus)+len(caus) == 0 {
 			return nil
