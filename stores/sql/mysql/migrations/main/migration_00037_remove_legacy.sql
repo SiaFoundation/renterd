9186974e50cefd1629d10b1bbe2b31c0f00f33be
Chris Schinnerl <chris@sia.tech>
2025-07-03 14:40:06 +0200

    remove v1 contracts in migration

--- stores/sql/mysql/migrations/main/migration_00037_remove_legacy.sql
+++ stores/sql/mysql/migrations/main/migration_00037_remove_legacy.sql
@@ -1,6 +1,7 @@
-ALTER TABLE hosts DROP INDEX idx_hosts_net_address;
+ALTER TABLE `hosts` DROP INDEX idx_hosts_net_address;
 ALTER TABLE `hosts` DROP COLUMN net_address;
 ALTER TABLE `hosts` DROP COLUMN price_table;
 ALTER TABLE `hosts` DROP COLUMN price_table_expiry;
 
+DELETE FROM `contracts` WHERE `v2` IS FALSE;
 ALTER TABLE `contracts` DROP COLUMN `v2`;
