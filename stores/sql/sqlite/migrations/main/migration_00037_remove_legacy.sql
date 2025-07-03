94c5590887c48b0023432d3330146cc030b02e1a
Chris Schinnerl <chris@sia.tech>
2025-07-03 14:40:06 +0200

    remove v1 contracts in migration

--- stores/sql/sqlite/migrations/main/migration_00037_remove_legacy.sql
+++ stores/sql/sqlite/migrations/main/migration_00037_remove_legacy.sql
@@ -3,4 +3,5 @@ ALTER TABLE `hosts` DROP COLUMN `net_address`;
 ALTER TABLE `hosts` DROP COLUMN `price_table`;
 ALTER TABLE `hosts` DROP COLUMN `price_table_expiry`;
 
+DELETE FROM `contracts` WHERE `v2` IS FALSE;
 ALTER TABLE `contracts` DROP COLUMN `v2`;
