84db832aa5918c1f20cf6d9576b4f247ba846bc3
Chris Schinnerl <chris@sia.tech>
2025-07-22 15:56:25 +0200

    migrations

--- stores/sql/mysql/migrations/main/migration_00038_broadcasted_transactions.sql
+++ stores/sql/mysql/migrations/main/migration_00038_broadcasted_transactions.sql
@@ -1,1 +1,12 @@
 DROP TABLE `wallet_locked_outputs`;
+CREATE TABLE wallet_broadcasted_txnsets (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `created_at` datetime(3) DEFAULT NULL,
+  `block_id` varbinary(32) NOT NULL,
+  `height` bigint unsigned NOT NULL,
+  `txn_set_id` varbinary(32) NOT NULL,
+  `raw_transactions` longblob NOT NULL, -- binary serialized transaction set
+  PRIMARY KEY (`id`),
+  UNIQUE KEY wallet_broadcasted_txnsets_txn_set_id (`txn_set_id`),
+  KEY wallet_broadcasted_txnsets_created_at (`created_at`)
+);
--- stores/sql/sqlite/migrations/main/migration_00038_broadcasted_transactions.sql
+++ stores/sql/sqlite/migrations/main/migration_00038_broadcasted_transactions.sql
@@ -1,1 +1,11 @@
 DROP TABLE `wallet_locked_outputs`;
+CREATE TABLE wallet_broadcasted_txnsets (
+    `id` integer PRIMARY KEY AUTOINCREMENT,
+    `created_at` datetime NOT NULL,
+    `block_id` blob NOT NULL,
+    `height` integer NOT NULL,
+	`txn_set_id` blob NOT NULL,
+	`raw_transactions` BLOB NOT NULL -- binary serialized transaction set
+);
+CREATE UNIQUE INDEX wallet_broadcasted_txnsets_txn_set_id ON wallet_broadcasted_txnsets(txn_set_id);
+CREATE INDEX wallet_broadcasted_txnsets_created_at ON wallet_broadcasted_txnsets(created_at);
